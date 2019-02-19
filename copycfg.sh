4a10f06b463380361dc8e1fb9b464862bf581afd
Misha Behersky <bmwant@gmail.com>
2019-02-19 14:13:58 +0200

    Copy ZSH config :maple_leaf:

--- copycfg.sh
+++ copycfg.sh
@@ -8,5 +8,8 @@ function info() {
   echo -e "${CYAN}$@${NC}" 
 }
 
-info "Copying ${HOME}/.bash_profile to $1/.bash_profile"
+info "Copying ${HOME}/.bash_profile to $1/.bash_profile..."
 cp "${HOME}/.bash_profile" "$1/.bash_profile"
+
+info "Copying ${HOME}/.zshrc to $1/.zshrc..."
+cp "${HOME}/.zshrc" "$1/.zshrc"
